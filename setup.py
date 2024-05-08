2ee9b120c1fc2bed0f57b1a18b5f81d71a50fa64
“Aariz <aariz120@gmail.com>
2024-05-08 14:31:44 +0530

    file folder added

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
-""
+from setuptools import find_packages, setup
+
+setup(
+    name="qasystem",
+    version="0.0.1",
+    author="aariz",
+    author_email="aariz120@ineuron.ai",
+    packages=find_packages(),
+    install_requires=["langchain","langchainhub","bs4","tiktoken","openai","boto3==1.34.37","langchain_community","chromadb","awscli",
+"streamlit",
+"pypdf",
+"faiss-cpu"]
+)
+    
